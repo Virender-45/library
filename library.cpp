fffa9bd7fc5e1f02f1e88ffde5f1ba3469b8f55d
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2024-12-25 00:28:38 +0530

    Update library.cpp

--- library.cpp
+++ library.cpp
@@ -17,28 +17,30 @@ public:
     };
     Book *booksHave = new Book[100];
 
-
     void displayMenu();
     void addBook();
     void displayBooks();
     void bookSearch();
 };
-void Library::bookSearch()
+
+void Library::bookSearch() // Not working
 {
     string t;
     cout << "Enter the title of the book you are looking for : ";
-    cin >> t;   
+    getline(cin, t);
 
+    bool found = false;
     for (int i = 0; i < 3; i++)
     {
         if (booksHave[i].title == t)
         {
-            cout << "Your book with name " << booksHave[i].title << " is found and available." << endl;
+            cout << "Your book with name " << booksHave[i].title << " is found .";
+            found = true;
         }
-        else
-        {
-            cout << "Book found but not available." << endl;
-        }
+    }
+    if (!found)
+    {
+        cout << "The book with title '" << t << "' not found" << endl;
     }
 }
 void Library::displayBooks()
@@ -47,15 +49,18 @@ void Library::displayBooks()
     booksHave[1] = {"dhiman", "bhau", 76427, 1};
     booksHave[2] = {"chandel", "brother", 35845, 0};
 
+    cout << "We have following books in our library:--" << endl
+         << endl;
+
     for (int i = 0; i < 3; i++)
     {
         if (booksHave[i].isAvailable == 1)
         {
-            cout << "Book " << i + 1 << " with title " << booksHave[i].title << " is available. " << endl;
+            cout << "Book " << i + 1 << " : with title " << booksHave[i].title << " is available. " << endl;
         }
         else
         {
-            cout << "Book " << i + 1 << " with title " << booksHave[i].title << " is not available. " << endl;
+            cout << "Book " << i + 1 << " : with title " << booksHave[i].title << " is not available. " << endl;
         }
     }
 }
@@ -85,20 +90,43 @@ void Library::displayMenu()
 {
     cout << "<----------------Chose an option?---------------->" << endl;
     cout << "(1) Add a new book." << endl;
-    cout << "(2) Search for a book by title." << endl;
-    cout << "(3) Display all books." << endl;
+    cout << "(2) Display all books." << endl;
+    cout << "(3) Search for a book by title." << endl;
     cout << "(4) Borrow a book." << endl;
     cout << "(5) Return a book." << endl;
     cout << "(6) Exit the library." << endl;
 }
 int main()
 {
+    int choice;
+    
     Library user1;
-    user1.bookSearch();
+    user1.displayMenu();
+    cout<<"Enetr choice : ";
+    // cin>>choice;
+    do
+    {
+        switch (choice)
+        {
+        case 1:
+            user1.addBook();
+            break;
+        case 2:
+            user1.displayBooks();
+            break;
+        case 6:
+        cout<<"Exiting...";
+        return 0;
 
-    int ch;
-    cout << "\n\nPress any key to continue....";
-    ch = getch();
+        default:
+        cout<<"Invaild Input";
+            break;
+        }
+    } while (choice != 6);
+
+    // int ch;
+    // cout << "\n\nPress any key to continue....";
+    // ch = getch();
 
     return 0;
 }
