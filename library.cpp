9f04198b3edb3751ffae50db6399136be8baf209
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2024-12-25 05:15:47 +0530

    Update library.cpp

--- library.cpp
+++ library.cpp
@@ -3,12 +3,10 @@
 
 using namespace std;
 
-class Library
-{
+class Library{
 
 public:
-    struct Book
-    {
+    struct Book{
         string title;
         string author;
         int isbn;
@@ -22,53 +20,44 @@ public:
     void bookSearch();
     void borrowBook();
 };
-void Library::borrowBook()
-{
+void Library::borrowBook(){
     displayBooks();
     int choice;
     cout << "\nWhich one do you want : ";
     cin >> choice;
 
-    for (int i = 0; i < choice; i++)
-    {
-        if (choice == i + 1)
-        {
-            if (booksHave[i].isAvailable == 1)
-            {
+    for (int i = 0; i < choice; i++){
+        if (choice == i + 1){
+            if (booksHave[i].isAvailable == 1){
                 cout << "Book is available and issued to you." << endl;
                 cout << "Here is your book......." << endl;
                 exit(0);
             }
-            else
-            {
+            else{
                 cout << "Book is unavailable at this time!" << endl;
                 exit(0);
             }
         }
     }
 }
-void Library::bookSearch() // Not working
-{
+void Library::bookSearch(){     // Not working
+
     string t;
     cout << "Enter the title of the book you are looking for : ";
     getline(cin, t);
 
     bool found = false;
-    for (int i = 0; i < 3; i++)
-    {
-        if (booksHave[i].title == t)
-        {
+    for (int i = 0; i < 3; i++){
+        if (booksHave[i].title == t){
             cout << "Your book with name " << booksHave[i].title << " is found .";
             found = true;
         }
     }
-    if (!found)
-    {
+    if (!found){
         cout << "The book with title '" << t << "' not found" << endl;
     }
 }
-void Library::displayBooks()
-{
+void Library::displayBooks(){
     booksHave[0] = { "rana", "saab", 76567, 1 };
     booksHave[1] = { "dhiman", "bhau", 76427, 0 };
     booksHave[2] = { "chandel", "brother", 723, 1 };
@@ -77,21 +66,18 @@ void Library::displayBooks()
     cout << "We have following books in our library:--" << endl
         << endl;
 
-    for (int i = 0; i < 4; i++)
-    {
+    for (int i = 0; i < 4; i++){
         cout << "Book " << i + 1 << " : with title '" << booksHave[i].title << "'" << endl;
     }
-
 }
 
-void Library::addBook()
-{
+void Library::addBook(){
     int n;
     cout << "Enter the number of books you want to add : ";
     cin >> n;
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
+
         cout << "\nEnter book title for book " << i + 1 << " : ";
         cin >> booksHave[i].title;
         cout << "Enter book author for book : ";
@@ -100,14 +86,13 @@ void Library::addBook()
         cin >> booksHave[i].isbn;
         booksHave[i].isAvailable = 1;
     }
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         cout << "\nThe book with title '" << booksHave[i].title << "' added." << endl;
     }
     exit(0);
 }
-void Library::displayMenu()
-{
+void Library::displayMenu(){
+
     cout << "<----------------Chose an option?---------------->" << endl;
     cout << "(1) Add a new book." << endl;
     cout << "(2) Display all books." << endl;
@@ -117,15 +102,13 @@ void Library::displayMenu()
     cout << "(6) Exit the library." << endl;
     cout << "Enter Choice : ";
 }
-int main()
-{
+int main(){
     int choice;
 
     Library user1;
     user1.displayMenu();
     cin >> choice;
-    do
-    {
+    do{
         switch (choice)
         {
         case 1:
